3966c0c5d6c1d099f0b379f8dfec4c834adf0dd4
Matt Hayes <mhayes@mhayes-mac.local>
2015-09-10 21:54:20 -0400

    adding photos

--- andcaroline/ringPhotos/photos.js
+++ andcaroline/ringPhotos/photos.js
@@ -5,20 +5,19 @@
 
     function RingPhotosCtrl() {
         this.name = "Ring photos controller";
-        this.photos = ['/images/IMG_3186.JPG'];
-
-        for(var i = 3187; i < 3202; i++) {
-            this.photos.push('/images/IMG_' + i + '.JPG');
-        }
     }
 
     function ringPhotos() {
         return {
             restrict: 'E',
-            controller: 'RingPhotosCtrl',
+            controller: 'RingPhotosCtrl as photo',
             templateUrl: 'ringPhotos/photos.html',
             link: function(scope, element) {
-                
+                photo.photos = [];
+
+                for(var i = 3186; i < 3202; i++) {
+                    photo.photos.push('/images/IMG_' + i + '.JPG');
+                }
             }
         };
     }
